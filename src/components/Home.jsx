baa206a1bc5723ea569fd167eb58a85e25415968
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-10-27 22:52:41 -0400

    stating product component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="hero">
+      <div class="card text-white border-0">
+        <img src={require('../assets/pic.jpg')} class="card-img" alt="Background" height='650px' />
+        <div class="card-img-overlay d-flex flex-column justify-content-center
+        ">
+        <div className="container">
+          <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+          <p class="card-text lead fs-2">
+            CHECK OT ALL THE TRENDS
+          </p>
+         
+
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+export default function Products() {
+    
+const [data,setData] = useState([])
+const [filter,setFilter] = useState(data)
+const [loading,setLoading] = useState(false)
+
+  return (
+    <div>Products</div>
+  )
+}
