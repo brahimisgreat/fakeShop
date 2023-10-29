70aa5b062ad7629e44691da4adfe11a8f3fb7a1f
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-10-28 20:20:39 -0400

    product filters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Products from "./Products";
 
 export default function Home() {
   return (
     <div className="hero">
-      <div class="card text-white border-0">
-        <img src={require('../assets/pic.jpg')} class="card-img" alt="Background" height='650px' />
-        <div class="card-img-overlay d-flex flex-column justify-content-center
+      <div className="card text-white border-0">
+        <img src={require('../assets/pic.jpg')} className="card-img" alt="Background" height='650px' />
+        <div className="card-img-overlay d-flex flex-column justify-content-center
         ">
         <div className="container">
-          <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-          <p class="card-text lead fs-2">
+          <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+          <p className="card-text lead fs-2">
             CHECK OT ALL THE TRENDS
           </p>
          
@@ -17,6 +18,7 @@ export default function Home() {
         </div>
         </div>
       </div>
+      <Products />
     </div>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,89 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Products() {
-    
-const [data,setData] = useState([])
-const [filter,setFilter] = useState(data)
-const [loading,setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
 
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+        console.log(filter);
+      }
+      return () => {};
+    };
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return <>Loading...</>;
+  };
+const filterProduct= (cat) => {
+    const updatedList = data.filter((x)=>x.category === cat)
+    setFilter(updatedList)
+}
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)}>All</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(`men's clothing`)}>Men's Clothing</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(`women's clothing`)}>
+            Women's Clothing
+          </button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(`jewelery`)}>
+            Jewelery Clothing
+          </button>
+          <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(`electronics`)}>
+            Electronics
+          </button>
+        </div>
+        {filter.map((product) => {
+          return (
+            
+              <div className="col-md-3 mb-4" key={product.id}>
+                <div  className="card h-100 text-center p-4" style={{width: "18rem"}} >
+                  <img src={product.image} className="card-img-top" alt={product.title} style={{height:'13.8rem'}}/>
+                  <div className="card-body" >
+                    <h5 className="card-title mb-0">{product.title.substring(0, 12)}...</h5>
+                    <p className="card-text lead fw-bold">
+                      ${product.price}
+                    </p>
+                    <Link href="#" className="btn btn-outline-dark">
+                      Go somewhere
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            
+          );
+        })}
+      </>
+    );
+  };
   return (
-    <div>Products</div>
-  )
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">
+              Lastest Products
+            </h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
 }
